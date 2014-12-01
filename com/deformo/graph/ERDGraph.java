d78df9fb7940d63a71e6431214b9ccbe1e17e8b2
radityopw <radityo.p.w@gmail.com>
2014-12-02 03:53:20 +0700

    fixing edge pada ERDGraph

--- com/deformo/graph/ERDGraph.java
+++ com/deformo/graph/ERDGraph.java
@@ -19,9 +19,6 @@ import com.deformo.node.MultivaluedAttributeNode;
 import com.deformo.node.NoteNode;
 import com.deformo.node.RelationNode;
 import com.deformo.node.WeakEntityNode;
-import com.deformo.shared.ArrowHead;
-import com.deformo.shared.BentStyle;
-import com.deformo.shared.LineStyle;
 import java.awt.geom.Point2D;
 
 /**
@@ -50,7 +47,7 @@ public class ERDGraph extends Graph {
 
    private static final Node[] NODE_PROTOTYPES = new Node[8];
 
-   private static final Edge[] EDGE_PROTOTYPES = new Edge[7];
+   private static final Edge[] EDGE_PROTOTYPES = new Edge[2];
 
    static
    {
@@ -63,37 +60,9 @@ public class ERDGraph extends Graph {
       NODE_PROTOTYPES[6] = new RelationNode();
       NODE_PROTOTYPES[7] = new NoteNode();
 
-      ClassRelationshipEdge dependency = new ClassRelationshipEdge();
-      dependency.setLineStyle(LineStyle.DOTTED);
-      dependency.setEndArrowHead(ArrowHead.V);
-      EDGE_PROTOTYPES[0] = dependency;
-      ClassRelationshipEdge inheritance = new ClassRelationshipEdge();
-      inheritance.setBentStyle(BentStyle.VHV);
-      inheritance.setEndArrowHead(ArrowHead.TRIANGLE);
-      EDGE_PROTOTYPES[1] = inheritance;
-
-      ClassRelationshipEdge interfaceInheritance = new ClassRelationshipEdge();
-      interfaceInheritance.setBentStyle(BentStyle.VHV);
-      interfaceInheritance.setLineStyle(LineStyle.DOTTED);
-      interfaceInheritance.setEndArrowHead(ArrowHead.TRIANGLE);
-      EDGE_PROTOTYPES[2] = interfaceInheritance;
-
-      ClassRelationshipEdge association = new ClassRelationshipEdge();
-      association.setBentStyle(BentStyle.HVH);
-      association.setEndArrowHead(ArrowHead.V);
-      EDGE_PROTOTYPES[3] = association;
-
-      ClassRelationshipEdge aggregation = new ClassRelationshipEdge();
-      aggregation.setBentStyle(BentStyle.HVH);
-      aggregation.setStartArrowHead(ArrowHead.DIAMOND);
-      EDGE_PROTOTYPES[4] = aggregation;
-
-      ClassRelationshipEdge composition = new ClassRelationshipEdge();
-      composition.setBentStyle(BentStyle.HVH);
-      composition.setStartArrowHead(ArrowHead.BLACK_DIAMOND);
-      EDGE_PROTOTYPES[5] = composition;
-
-      EDGE_PROTOTYPES[6] = new NoteEdge();
+      ClassRelationshipEdge edge = new ClassRelationshipEdge();
+      EDGE_PROTOTYPES[0] = edge;
+      EDGE_PROTOTYPES[1] = new NoteEdge();
    }
     
 }
